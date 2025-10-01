e006de7916d919b29f48ad50ecae3f82504d9716
Satya Prakash <satyaprakash@styava.com>
2025-10-01 16:32:44 +0530

    initial commit

---
+++ src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,12 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String userId;
+    private String source;
+    private String destination;
+
+    private Date dateOfArrival;
+    private  Train train;
+}
