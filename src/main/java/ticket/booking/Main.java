62a7392b66714cefda5c6d71cca992f548657043
Satya Prakash <satyaprakash@styava.com>
2025-10-02 11:56:22 +0530

    chore: added trainservice

--- src/main/java/ticket/booking/Main.java
+++ src/main/java/ticket/booking/Main.java
@@ -1,9 +1,14 @@
 package ticket.booking;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+        List<Integer> l= Arrays.asList(1,2,3,4,5);
+
 
     }
 
--- src/main/java/ticket/booking/entities/Train.java
+++ src/main/java/ticket/booking/entities/Train.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Train {
     private String trainId;
-    private String trainNo;
+    private Integer trainNo;
     private List<List<Integer>> seats;
 
     private Map<String, Time> stationTimes;
